0be2e968aca4384f88b52e3caebb05d08615f90e
Danila <danila-18@mail.ru>
2019-01-25 00:37:58 +0300

    Dish form completed

--- src/app/features/dishes/dish/dish.component.ts
+++ src/app/features/dishes/dish/dish.component.ts
@@ -16,9 +16,8 @@ export class DishComponent implements OnInit {
 
   products: IDishProduct[] = [];
   dish: IDish;
-  dishForm: FormArray;
+  dishForm: FormGroup;
   productsAll: IProduct[] = PRODUCT_DATA;
-  dishTitles = DISH_TITLES;
   dishRelations = DISH_RELATIONS;
 
   constructor(private activatedRoute: ActivatedRoute) {
@@ -27,21 +26,24 @@ export class DishComponent implements OnInit {
   ngOnInit() {
     const dishID = +this.activatedRoute.snapshot.params.dish_id;
     this.dish = DISH_TITLES.find(dish => dish.dish_id === dishID);
-
     this.products = <IDishProduct[]>this.dishRelations
       .filter(d => d.dish_id === dishID)
       .map(d => {
         return <IDishProduct>{...d, ...this.productsAll.find(p => p.id === d.product_id)};
       });
 
-    this.dishForm = new FormArray(
-      this.products.map(prod => {
-        return new FormGroup({
-          product_id: new FormControl(prod.product_id),
-          amount: new FormControl(prod.amount)
-        });
-      })
-    );
+    this.dishForm = new FormGroup({
+      title: new FormControl(this.dish.title),
+      description: new FormControl(this.dish.description),
+      dishes: new FormArray(
+        this.products.map(prod => {
+          return new FormGroup({
+            product_id: new FormControl(prod.product_id),
+            amount: new FormControl(prod.amount)
+          });
+        })
+      )
+    });
   }
 
   onSave() {
@@ -53,12 +55,12 @@ export class DishComponent implements OnInit {
   }
 
   onAddProduct() {
-    this.dishForm.controls.push(
-      new FormGroup({
-        product_id: new FormControl(),
-        amount: new FormControl()
-      })
-    );
+    const newProduct = new FormGroup({
+      product_id: new FormControl(),
+      amount: new FormControl()
+    });
+    newProduct.setParent(this.dishForm);
+    (<FormArray>this.dishForm.controls['dishes']).push(newProduct);
   }
 
 }
