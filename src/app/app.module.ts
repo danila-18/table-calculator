1d2ef0f1b889a799099606a61ab9a38e08855992
Danila <danila-18@mail.ru>
2019-03-30 11:43:02 +0300

    Base href

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import {AppComponent} from './app.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
+import {APP_BASE_HREF} from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule,
     HttpClientModule],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap: [AppComponent],
 })
 export class AppModule {
--- src/app/features/banquets/banquets.component.ts
+++ src/app/features/banquets/banquets.component.ts
@@ -16,22 +16,6 @@ export interface IBanquetRelation {
   dish_count: number;
 }
 
-export const BANQUETS_TABLE: IBanquet[] = [
-  {banquet_id: 1, date: '12-12-2012', description: 'Застолье всех местных22', price: 3500, title: 'Название для застолья'},
-  {banquet_id: 2, date: '12-12-2012', description: 'Застолье всех местных2', price: 2512, title: 'Название для застолья'},
-  {banquet_id: 3, date: '12-12-2012', description: 'Застолье всех местных3', price: 4545, title: 'Название для застолья'},
-  {banquet_id: 4, date: '12-12-2012', description: 'Застолье всех местных4', price: 9045, title: 'Название для застолья'},
-  {banquet_id: 5, date: '12-12-2012', description: 'Застолье всех местных55', price: 3290, title: 'Название для застолья'},
-];
-
-export const BANQUETS_RELATIONS: IBanquetRelation[] = [
-  {id: 1, banquet_id: 1, dish_id: 1, dish_count: 4},
-  {id: 2, banquet_id: 1, dish_id: 2, dish_count: 1},
-  {id: 3, banquet_id: 1, dish_id: 3, dish_count: 3},
-  {id: 4, banquet_id: 2, dish_id: 5, dish_count: 6},
-  {id: 5, banquet_id: 2, dish_id: 3, dish_count: 4},
-];
-
 @Component({
   selector: 'app-banquets',
   templateUrl: './banquets.component.html',
@@ -39,7 +23,6 @@ export const BANQUETS_RELATIONS: IBanquetRelation[] = [
 })
 export class BanquetsComponent implements OnInit {
 
-  tableData: IBanquet[] = [];
   banquets: IBanquet[] = [];
   columns = [
     {
